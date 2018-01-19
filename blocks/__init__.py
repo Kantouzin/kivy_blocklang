a574862fc919269d0923918d334916588b64f0b8
Kantouzin <kantouzin0113@gmail.com>
2018-01-19 10:24:12 +0900

    Update "blocks/__init__.py"

--- blocks/__init__.py
+++ blocks/__init__.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 # coding: utf-8
 
+DISTANCE_RANGE = 20
+
 from blocks.function_block import PrintBlock
 from blocks.nest_block import IfBlock, ClassBlock, DefineBlock
 from blocks.argument_block import ArgumentBlock
@@ -9,5 +11,3 @@ from blocks.declare_block import DeclareBlock
 from kivy.config import Config
 
 Config.set('input', 'mouse', 'mouse,multitouch_on_demand')
-
-DISTANCE_RANGE = 20
